abd53e1937874ee2cd2ffd6bb913a3015163db8c
Szymon Kolanowski <szymonkolanowski@op.pl>
2021-10-07 08:35:19 +0200

    change

---
+++ Najlepsi strzelcy/Najlepsi strzelcy/AllLists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Najlepsi_strzelcy
+{
+	public class AllLists
+	{
+		public List<Scorer> Scorers { get; set; }
+
+		public AllLists()
+		{
+			Scorers = new List<Scorer>();
+		}
+	}
+}
